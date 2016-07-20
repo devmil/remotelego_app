fab11d12eb586a6850aab731295f71533388595c
fexdev <florian.wachs@live.de>
2016-07-20 20:44:16 +0200

    fix: reset knob position on window resize

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -1,43 +1,50 @@
-import { Component, Input, ElementRef, ViewChild, AfterViewInit, HostListener } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit, HostListener, OnDestroy } from '@angular/core';
 import { LegoCar } from "./../shared/LegoCar"
 
 @Component({
-  moduleId: module.id,
-  selector: 'car-control',
-  templateUrl: 'control.component.html',
-  styleUrls: ['control.component.css']
+	moduleId: module.id,
+	selector: 'car-control',
+	templateUrl: 'control.component.html',
+	styleUrls: ['control.component.css']
 })
-export class CarControlComponent implements AfterViewInit {
+export class CarControlComponent implements AfterViewInit, OnDestroy {
     @Input()
-    model : LegoCar;
+    model: LegoCar;
     @ViewChild('directioncontrol')
-    directionControl : ElementRef;
+    directionControl: ElementRef;
     @ViewChild('directioncontrol_knob')
-    directionControlKnob : ElementRef;
+    directionControlKnob: ElementRef;
 
-    isTouchActive : boolean;
-    lastTouch : any;
+    isTouchActive: boolean;
+    lastTouch: any;
+	resizeHandler: any;
 
     constructor() {
     }
 
     ngAfterViewInit() {
         this.setKnobPosFromSpeedAndSteer(0, 0);
+		this.resizeHandler = () => this.setKnobPosFromSpeedAndSteer(0, 0);
+		window.addEventListener("resize", this.resizeHandler);
     }
+
+	ngOnDestroy() {
+		window.removeEventListener("resize", this.resizeHandler);
+	}
 
     @HostListener('touchstart', ['$event'])
     @HostListener('mousedown', ['$event'])
     onDirectionControlTouchStart(ev) {
 		ev.preventDefault();
 		this.isTouchActive = true;
-        if(ev.changedTouches) {
+        if (ev.changedTouches) {
             this.lastTouch = ev.changedTouches[0];
         } else {
             this.lastTouch = ev;
         }
-	    window.requestAnimationFrame(() => this.handleTouch());
+		window.requestAnimationFrame(() => this.handleTouch());
 	}
-	
+
     @HostListener('touchend', ['$event'])
     @HostListener('mouseup', ['$event'])
 	onDirectionControlTouchEnd(ev) {
@@ -46,7 +53,7 @@ export class CarControlComponent implements AfterViewInit {
 		this.lastTouch = null;
 		window.requestAnimationFrame(() => this.handleTouch());
 	}
-	
+
     @HostListener('touchcancel', ['$event'])
     @HostListener('mouseout', ['$event'])
 	onDirectionControlTouchCancel(ev) {
@@ -60,50 +67,50 @@ export class CarControlComponent implements AfterViewInit {
     @HostListener('mousemove', ['$event'])
 	onDirectionControlTouchMove(ev) {
 		ev.preventDefault();
-		if(this.isTouchActive) {
-            if(ev.changedTouches) {
-			    this.lastTouch = ev.changedTouches[0];
+		if (this.isTouchActive) {
+            if (ev.changedTouches) {
+				this.lastTouch = ev.changedTouches[0];
             } else {
                 this.lastTouch = ev;
             }
-		    window.requestAnimationFrame(() => this.handleTouch());
+			window.requestAnimationFrame(() => this.handleTouch());
 		}
 	}
-	
-	handleTouch(){
-		if(this.lastTouch) {
+
+	handleTouch() {
+		if (this.lastTouch) {
 			this.setKnobPosFromTouch(this.lastTouch);
 		} else {
 			this.setKnobPosFromSpeedAndSteer(0, 0);
 		}
 	}
 
-    setKnobPosFromSpeedAndSteer(speedPercent : number, steerPercent : number) {
+    setKnobPosFromSpeedAndSteer(speedPercent: number, steerPercent: number) {
 		var availableWidth = this.directionControl.nativeElement.offsetWidth - this.directionControlKnob.nativeElement.offsetWidth;
 		var availableHeight = this.directionControl.nativeElement.offsetHeight - this.directionControlKnob.nativeElement.offsetHeight;
-		
+
 		var originOffsetX = this.directionControl.nativeElement.offsetLeft + this.directionControlKnob.nativeElement.offsetWidth / 2;
 		var originOffsetY = this.directionControl.nativeElement.offsetTop + this.directionControlKnob.nativeElement.offsetHeight / 2;
 
-		var offsetX = originOffsetX + ((steerPercent + 100) * availableWidth) / 200; 
-		var offsetY = originOffsetY + ((speedPercent + 100) * availableHeight) / 200; 
+		var offsetX = originOffsetX + ((steerPercent + 100) * availableWidth) / 200;
+		var offsetY = originOffsetY + ((speedPercent + 100) * availableHeight) / 200;
 
-		this.directionControlKnob.nativeElement.style.transform = 
+		this.directionControlKnob.nativeElement.style.transform =
 			"translate3d(" + (offsetX - originOffsetX) + "px," + (offsetY - originOffsetY) + "px," + "0px)";
-		
+
 		console.log("setting knob pos from values: speed=" + speedPercent + ", steering=" + steerPercent);
-		
+
 		this.model.steering = steerPercent;
 		this.model.speed = speedPercent * -1;
 	}
-	
-	setKnobPosFromTouch(touch : any) {
+
+	setKnobPosFromTouch(touch: any) {
 		var availableWidth = this.directionControl.nativeElement.offsetWidth - this.directionControlKnob.nativeElement.offsetWidth;
 		var availableHeight = this.directionControl.nativeElement.offsetHeight - this.directionControlKnob.nativeElement.offsetHeight;
-		
+
 		var originOffsetX = this.directionControl.nativeElement.offsetLeft + this.directionControlKnob.nativeElement.offsetWidth / 2;
 		var originOffsetY = this.directionControl.nativeElement.offsetTop + this.directionControlKnob.nativeElement.offsetHeight / 2;
-		
+
 		var translateX = touch.pageX - originOffsetX;
 		var translateY = touch.pageY - originOffsetY;
 
@@ -111,17 +118,17 @@ export class CarControlComponent implements AfterViewInit {
 		translateX = Math.min(Math.max(translateX, 0), availableWidth);
 		translateY = Math.min(Math.max(translateY, 0), availableHeight);
 
-		this.directionControlKnob.nativeElement.style.transform = 
+		this.directionControlKnob.nativeElement.style.transform =
 			"translate3d(" + (translateX) + "px," + (translateY) + "px," + "0px)";
-		
+
 		var xPercent = ((translateX) * 100) / availableWidth;
 		var yPercent = ((translateY) * 100) / availableHeight;
-		
+
 		var steer = (xPercent * 2) - 100;
 		var speed = (yPercent * 2) - 100;
-		
+
 		console.log("setting knob pos from touch: speed=" + speed + ", steering=" + steer);
-		
+
 		this.model.steering = steer;
 		this.model.speed = speed * -1;
 	}
